8b635c30205d33fd041456214f385a6c8669516b
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-03-02 19:55:01 +0700

    Initial Commit

---
+++ Podcast/PodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastApp.swift
+//  Podcast
+//
+//  Created by Tung Vu Duc on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct PodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
